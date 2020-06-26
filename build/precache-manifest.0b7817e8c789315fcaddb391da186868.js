91fd35b77042fdea45c3e4f50fa7041b4b09de4b
Jitendra Yadav <jitendra.yadav@xsuite.asia>
2020-06-26 13:42:51 +0530

    Add deploy

---
+++ build/precache-manifest.0b7817e8c789315fcaddb391da186868.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "09c1895ca167c86f2db5",
+    "url": "/repository-search/static/css/main.a86d7526.chunk.css"
+  },
+  {
+    "revision": "09c1895ca167c86f2db5",
+    "url": "/repository-search/static/js/main.09c1895c.chunk.js"
+  },
+  {
+    "revision": "25ea3454082c64426b2c",
+    "url": "/repository-search/static/js/runtime~main.25ea3454.js"
+  },
+  {
+    "revision": "9972556e904a40f27581",
+    "url": "/repository-search/static/js/2.9972556e.chunk.js"
+  },
+  {
+    "revision": "c9a5969730e38346419c3675c63c55aa",
+    "url": "/repository-search/index.html"
+  }
+];
